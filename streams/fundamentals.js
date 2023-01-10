330bdf650c407cb04dc8befdc3ed0d6a807a75ee
João <joaomanoeldev@gmail.com>
2023-01-10 13:49:28 -0300

    Creating a readable stream

---
+++ streams/fundamentals.js
@@ -0,0 +1,21 @@
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, [])
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout)
