df96ef83eea5d32689abd5aa0c8204a2c99ed785
João <joaomanoeldev@gmail.com>
2023-01-10 00:25:59 -0300

    Creating and listing routes (HTTP Methods)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+
+  if (method === 'GET' && url === '/users') {
+    return response.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return response.end('Criação de usuários')
+  }
+
+
   return response.end('Hello World')
 })
 
