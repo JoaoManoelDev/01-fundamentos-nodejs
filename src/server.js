2ceec1409c4f571895d25f7cc1426d09a0d64006
João <joaomanoeldev@gmail.com>
2023-01-10 00:47:42 -0300

    Saving users in memory (Headers)

--- src/server.js
+++ src/server.js
@@ -1,19 +1,29 @@
+// App Stateful
 import http from 'node:http'
+
+const users = []
 
 const server = http.createServer((request, response) => {
   const { method, url } = request
 
+
   if (method === 'GET' && url === '/users') {
-    return response.end('Listagem de usuários')
+    return response
+    .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return response.end('Criação de usuários')
-  }
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
 
+    return response.end()
+  }
 
   return response.end('Hello World')
 })
 
 server.listen(3333, () => console.log('Server running in port 3333.'))
-
