0f068891b2257a3d23d21610945f16f29fa2ccf1
João <joaomanoeldev@gmail.com>
2023-01-12 12:52:41 -0300

    Creating regex from parameters

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const users = database.select('users')
 
@@ -16,7 +17,7 @@ export const routes = [
 
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const { name, email } = request.body
 
@@ -31,4 +32,12 @@ export const routes = [
       return response.writeHead(201).end()
     }
   },
+
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler(request, response) {
+      return response.end()
+    },
+  }
 ]
