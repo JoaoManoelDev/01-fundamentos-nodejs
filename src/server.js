a8c31a8e6996e0e47c2f92e1bbfdc9c4d947c1a8
João <joaomanoeldev@gmail.com>
2023-01-12 13:14:23 -0300

    Routes with parameters (RegEx)

--- src/server.js
+++ src/server.js
@@ -10,10 +10,14 @@ const server = http.createServer(async (request, response) => {
   await json(request, response)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = request.url.match(route.path)
+
+    console.log(routeParams)
+
     return route.handler(request, response)
   }
 
